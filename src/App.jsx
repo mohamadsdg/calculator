09a8997ff3e11cc8fcc299f996a780b8161ba414
unknown <fkaymanesh@gmail.com>
2020-12-30 11:59:36 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+/** @jsxImportSource @emotion/react */
+import { css, jsx } from "@emotion/react";
+
+function App() {
+  return (
+    <div css={css`
+    background-color: green;
+
+    `}>
+      Hello World
+    </div>
+  );
+}
+
+export default App;
